ec118f0f638bbcba74a54abd6ea3450e47193445
Aayushi-Uniyal <auniyal_be24@thapar.edu>
2025-11-30 13:31:12 +0530

    Add Graph class with BFS implementation
    Implemented a Graph class with BFS traversal.

---
+++ Q1a.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Graph {
+public:
+    int V;
+    vector<vector<int>> adj;
+
+    Graph(int V) {
+        this->V = V;
+        adj.resize(V);
+    }
+
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);   // undirected
+    }
+
+    void BFS(int start) {
+        vector<bool> visited(V, false);
+        queue<int> q;
+
+        visited[start] = true;
+        q.push(start);
+
+        cout << "BFS: ";
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            cout << u << " ";
+
+            for (int v : adj[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.push(v);
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Graph g(5);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+
+    g.BFS(0);
+    return 0;
+}
